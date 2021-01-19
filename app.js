1fc481ec39de8731168e72d2b351b8a29a4ea8b2
karinacrmcldev <perfechate@gmail.com>
2021-01-20 00:18:45 +0200

    added bulshit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 
 
 
+
 function summa(firstNumber, secondNumber) {
     console.log(firstNumber(5) + secondNumber(1))
 }
@@ -21,6 +22,8 @@ function second() {
 }
 
 
+
+
 let years = document.getElementById("years")
 
 let months = document.getElementById("months")
@@ -28,9 +31,45 @@ let months = document.getElementById("months")
 let days = document.getElementById("days")
 
 function calculateAll() {
+
+
+    // if (!years.value && !months.value && !days.value) {
+    //     document.querySelectorAll('#error').forEach(element => {
+    //         element.style.display = 'block'
+    //     });
+    //     return
+    // }
+
+    if (!years.value) {
+        document.querySelector('.error-year').style.display = "block"
+
+        return
+
+    }
+
+    if (!months.value) {
+        document.querySelector('.error-month').style.display = "block"
+
+        return
+    }
+    if (!days.value) {
+        document.querySelector('.error-day').style.display = "block"
+
+        return
+
+    }
+
+    function removeall() {
+        document.querySelectorAll('#error').forEach(element => {
+            element.style.display = 'none'
+        });
+    }
+
+
+
+
     let sum = parseInt(years.value * 365) + parseInt(months.value * 31) + parseInt(days.value);
     document.getElementById("result").innerHTML = sum
-
 
 }
 document.getElementById("calcbtn").addEventListener('click', calculateAll);
